094680237781b0cf895f1ef9e543682123eba5f8
Justin Morris <justinjodymorris@gmail.com>
2018-04-30 13:02:20 -0700

    added paths to ensure connection on heroku

--- server.js
+++ server.js
@@ -16,5 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/users', (request, response) => {
+  client.query('SELECT * FROM users')
+    .then(result => response.send(result.rows))
+    .catch(console.error);
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
+
+
 
